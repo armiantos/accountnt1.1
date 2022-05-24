3a37178fd723509442f7e407d69a2ef968cbd661
Filippo Ciandy <filippo.ciandy10@gmail.com>
2022-05-24 13:47:57 -0600

    Added max warnings for client and server

--- server/routes/sampleEndpoint.ts
+++ server/routes/sampleEndpoint.ts
@@ -2,8 +2,6 @@ import express from 'express';
 
 export const sampleEndpointRouter = express.Router();
 
-let example;
-
 sampleEndpointRouter.get('/', (req, res) => {
     res.json({
         msg: 'Hello world',
