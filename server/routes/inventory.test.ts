f1f9e022b0b52a80a61531994d04fdd5c392b95b
Armianto Sumitro <armiantos@gmail.com>
2022-06-17 23:01:47 -0600

    Update inventory tests

--- server/routes/inventory.test.ts
+++ server/routes/inventory.test.ts
@@ -1,9 +1,32 @@
+import { InventoryItem } from '@prisma/client';
 import request from 'supertest';
 import { app } from '../app';
+import { prisma } from '../prisma/client';
+import { apples } from '../prisma/fixtures/inventoryItem';
+import { bob } from '../prisma/fixtures/user';
 
 describe('inventory', () => {
+    beforeAll(async () => {
+        prisma.user.create({
+            data: {
+                ...bob,
+                inventoryItems: {
+                    create: apples,
+                },
+            },
+        });
+    });
+
+    afterAll(async () => {
+        prisma.user.delete({ where: { id: bob.id } });
+    });
+
     it('GET / returns mock inventory items', async () => {
         const response = await request(app).get('/api/v1/inventory').expect(200);
-        expect(response.body).toEqual([]);
+        const body: [InventoryItem] = response.body;
+
+        expect(body.length).toEqual(1);
+        const apple = body.find((item) => item.name == apples.name);
+        expect(apple).not.toBeUndefined();
     });
 });
