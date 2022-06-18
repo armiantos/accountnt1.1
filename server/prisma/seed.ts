818193bcf21e69d3a0b9bba1a8f3218d8bc391a3
Armianto Sumitro <armiantos@gmail.com>
2022-06-17 22:21:39 -0600

    Format seed file

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,29 +1,29 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.upsert({
-    where: { email: "bob@prisma.io" },
-    update: {},
-    create: {
-      id: "74288096-dc31-4928-a11b-daa35282ae4b",
-      email: "bob@prisma.io",
-      name: "Bob Doyle",
-      inventoryItems: {
+    await prisma.user.upsert({
+        where: { email: 'bob@prisma.io' },
+        update: {},
         create: {
-          name: "Apples",
+            id: '74288096-dc31-4928-a11b-daa35282ae4b',
+            email: 'bob@prisma.io',
+            name: 'Bob Doyle',
+            inventoryItems: {
+                create: {
+                    name: 'Apples',
+                },
+            },
         },
-      },
-    },
-  });
+    });
 }
 
 main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
