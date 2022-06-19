ece6ff8f1b15f136ab3a2f4f2bdc1b86331d3e36
armiantos <43416725+armiantos@users.noreply.github.com>
2022-06-18 22:08:47 -0600

    Add tests for server (#9)
    * Install ts-jest
    
    * Install supertest
    
    * Separate app.listen from app module
    
    * Update start scripts
    
    * Add sampleEndpoint tests
    
    * Run tests on server
    
    * Format autogenerated config file

--- server/app.ts
+++ server/app.ts
@@ -1,19 +1,14 @@
-import express from 'express'
+import express from 'express';
 
-import {sampleEndpointRouter} from './routes/sampleEndpoint'
+import { sampleEndpointRouter } from './routes/sampleEndpoint';
 
-const app = express()
-const port = 3001
+export const app = express();
 
-const apiRouter = express.Router()
-apiRouter.use('/sample_endpoint', sampleEndpointRouter)
+const apiRouter = express.Router();
+apiRouter.use('/sample_endpoint', sampleEndpointRouter);
 
-app.use('/api/v1', apiRouter)
+app.use('/api/v1', apiRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    res.send('Hello World!');
+});
