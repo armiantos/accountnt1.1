967316a67c4e041c6df4eb8d2c992e746dcdcce0
Armianto Sumitro <armiantos@gmail.com>
2022-06-20 20:05:56 -0600

    Remove missing .env catch

--- server/config.ts
+++ server/config.ts
@@ -1,7 +1,3 @@
-if (!process.env.AUTH0_AUDIENCE || !process.env.AUTH0_ISSUER) {
-    throw new Error('Missing auth0 config');
-}
-
 const auth0Config = {
     audience: process.env.AUTH0_AUDIENCE || '',
     issuer: process.env.AUTH0_ISSUER || '',
